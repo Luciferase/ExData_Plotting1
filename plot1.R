cbeeb11a27cccee48d69f962f48f2de3a31377ed
Luciferase <f.alex.jacobs@gmail.com>
2015-04-09 08:08:31 +0200

    Create plot1.R

---
+++ plot1.R
@@ -0,0 +1,32 @@
+
+## load files
+
+electricPowerConsumption <- read.table("household_power_consumption.txt", sep = ";", header = TRUE, na.strings = "?")
+
+## reduce data to 2007-02-01 and 2007-02-02
+
+electricPowerConsumption <- subset(electricPowerConsumption, Date == "1/2/2007"| Date == "2/2/2007")
+
+## set Date as class date
+
+electricPowerConsumption$Date <- as.Date(electricPowerConsumption$Date, format = "%d/%m/%Y")
+
+## set time as class time
+
+electricPowerConsumption$Time <- strptime(electricPowerConsumption$Time, format = "%H:%M:%S")
+
+## Open Dev
+
+dev.cur()
+
+## Open png device
+
+png(filename = "plot1.png", width = 480, height = 480)
+
+## Draw Plot 1
+
+hist(electricPowerConsumption$Global_active_power, col = "red", main = "Global Active Power", xlab = "Global Active Power (kilowatts)")
+
+## Close dev
+
+dev.off()
