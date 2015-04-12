6c9d005b7d075b6808e30f0f0ef83cf08bcad48e
Luciferase <f.alex.jacobs@gmail.com>
2015-04-12 10:31:59 +0200

    Update plot3.R

--- plot3.R
+++ plot3.R
@@ -1,5 +1,5 @@
 ## This script needs to be run in the same folder as "exdata-data-household_power_consumption.zip"
-## This script outputs a png file, plot2.png for Exploratory Data Analysis Course Project 1
+## This script outputs a png file, plot3.png for Exploratory Data Analysis Course Project 1
 
 ## Unzip Data
 unzip("exdata-data-household_power_consumption.zip")
@@ -23,7 +23,7 @@ dev.cur()
 ## Open png device
 png(filename = "plot3.png", width = 480, height = 480)
 
-## Draw Plot 1
+## Draw Plot3
 plot(electricPowerConsumption$DateTime, electricPowerConsumption$Sub_metering_1, ylab = "Energy sub metering", type = "l", xlab = "")
 points(electricPowerConsumption$DateTime, electricPowerConsumption$Sub_metering_2, type = "l", col = "red")
 points(electricPowerConsumption$DateTime, electricPowerConsumption$Sub_metering_3, type = "l", col = "blue")
